ef79776df64c4b1e931de8636e1c6c4361b41235
krishpranav <krisna.pranav@gmail.com>
2023-06-14 19:48:02 +0530

    pygodzilla: utils

--- pygodzilla/utils/util.py
+++ pygodzilla/utils/util.py
@@ -7,7 +7,6 @@ import uuid
 from typing import Callable
 
 def get_next_available_port(port: int) -> int:
-
     def is_port_available() -> bool:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -18,10 +17,39 @@ def get_next_available_port(port: int) -> int:
         finally:
             sock.close()
 
-        while not is_port_available():
-            port += 1
-        
-        return port
-    
+    while not is_port_available():
+        port += 1
+
+    return port
+
+
 def unique_id() -> str:
     return str(uuid.uuid4()).replace("-", "")
+
+
+def current_time_us() -> int:
+    return int(time.time() * 1e6)
+
+
+def register_signal_term(sigterm_handler: Callable):
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    signal.signal(signal.SIGINT, original_sigint_handler)
+
+    if sys.platform == "win32":
+        signal.signal(signal.SIGBREAK, sigterm_handler)
+    else:
+        signal.signal(signal.SIGQUIT, sigterm_handler)
+        signal.signal(signal.SIGHUP, sigterm_handler)
+
+
+def log_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:  
+            logging.exception(exc)
+            return None
+
+    return wrapper
